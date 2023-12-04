2abcc60ef11621f2b40fc5a7dd04d154e17df63e
christopherflores9312 <84460061+christopherflores9312@users.noreply.github.com>
2023-12-03 20:42:26 -0700

    added procfile and prepared for heroku deployment

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3001;
 const AWS = require('aws-sdk');
 require('dotenv').config();
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -33,6 +35,12 @@ app.get('/download', (req, res) => {
         }
     });
 });
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back the index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
 
 app.listen(port, () => {
